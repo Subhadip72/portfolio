7f9b2f83fc873283dfcb2fffc913e6aab67e513b
Subhadip Chakraborty <subhadipc992@gmail.com>
2023-06-12 21:29:58 +0530

    fourth commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -16,8 +16,7 @@ const Contact = () => {
             Contact
           </p>
           <p className="text-gray-300 p-4 text-xl">
-            // Submit the form below or shoot me an email -
-            subhadipchakraborty193@gmail.com
+            // Submit the form below to contact me
           </p>
         </div>
         <input
